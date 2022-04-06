9c6974e6e122225e88c85eb24d1ebdd1e494fe83
ahmedgamalelazab <49871250+ahmedgamalelazab@users.noreply.github.com>
2022-04-06 13:04:20 +0300

    performance Boosted

--- MVVMProject/ViewModel/UserViewModel.cs
+++ MVVMProject/ViewModel/UserViewModel.cs
@@ -1,13 +1,8 @@
 ﻿using MVVMProject.Commands;
 using MVVMProject.Model;
 using MVVMProject.Services;
-using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
 
